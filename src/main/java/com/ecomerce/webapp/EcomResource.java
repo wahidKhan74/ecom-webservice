131816278b0f3dc637066fa11120210328a62ba0
waheed khan <wahid.khan74@gmail.com>
2020-12-12 15:04:37 +0000

    ecom web service setup

---
+++ src/main/java/com/ecomerce/webapp/EcomResource.java
@@ -0,0 +1,14 @@
+package com.ecomerce.webapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EcomResource {
+	
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(value = "name", defaultValue="World") String name) {
+		return "Hello , " + name;
+	}
+}
