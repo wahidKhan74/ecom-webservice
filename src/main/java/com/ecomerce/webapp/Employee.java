ff16eca20960f4a1b5c5f2846b2437f42d225cd7
Wahid <wahid.khan74@gmail.com>
2021-11-09 16:50:27 +0530

    Create Employee.java

---
+++ src/main/java/com/ecomerce/webapp/Employee.java
@@ -0,0 +1,27 @@
+public class Employee {
+ 
+    public Employee() {
+ 
+    }
+ 
+    public Employee(Integer id, String firstName, String lastName, String email) {
+        super();
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+  
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+ 
+    //Getters and setters
+ 
+    @Override
+    public String toString() {
+        return "Employee [id=" + id + ", firstName=" + firstName + ", 
+                lastName=" + lastName + ", email=" + email + "]";
+    }
+}
