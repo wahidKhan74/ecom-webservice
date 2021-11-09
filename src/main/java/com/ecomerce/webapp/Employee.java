9ae0060ba2be2ad418b89b2967eb82f81c95078c
waheed khan <wahid---@gmail.com>
2021-11-09 17:25:05 +0530

    fix package changes

--- src/main/java/com/ecomerce/webapp/Employee.java
+++ src/main/java/com/ecomerce/webapp/Employee.java
@@ -20,7 +20,35 @@ public class Employee {
     private String email;
  
     //Getters and setters
- 
+    public Integer getId() {
+        return this.id;
+    }
+    public String getFirstName() {
+        return this.firstName;
+    }
+    public String getLastName() {
+        return this.lastName;
+    }
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setId(Integer id) {
+        this.id= id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName= firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName= lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email= email;
+    }
+
     @Override
     public String toString() {
         return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
