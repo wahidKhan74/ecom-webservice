bfa15e3a264e80fe68affca40a6b2b618891f555
Wahid <wahid.khan74@gmail.com>
2021-11-09 17:00:01 +0530

    Update Employee.java

--- src/main/java/com/ecomerce/webapp/Employee.java
+++ src/main/java/com/ecomerce/webapp/Employee.java
@@ -23,7 +23,6 @@ public class Employee {
  
     @Override
     public String toString() {
-        return "Employee [id=" + id + ", firstName=" + firstName + ", 
-                lastName=" + lastName + ", email=" + email + "]";
+        return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
     }
 }
