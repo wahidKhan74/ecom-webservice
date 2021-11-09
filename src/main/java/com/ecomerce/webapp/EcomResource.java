2cbb33aeec5d18a1e69373889cdc3e11c94ea020
Wahid <wahid.khan74@gmail.com>
2021-11-09 12:07:02 +0530

    Update EcomResource.java

--- src/main/java/com/ecomerce/webapp/EcomResource.java
+++ src/main/java/com/ecomerce/webapp/EcomResource.java
@@ -14,7 +14,7 @@ public class EcomResource {
 	
 	@GetMapping("/")
 	public String index() {
-		return "Hello, Welcome ecommerce webservice application !";
+		return "Hello, Welcome ecommerce webservice application ! deployed with ci cd pipeine";
 	}
 	
 	@GetMapping("/hello")
