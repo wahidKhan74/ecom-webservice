c3e39c56fa0fe5a8676ca8f5d5cef736b5bb349e
Wahid <wahid.khan74@gmail.com>
2021-12-08 16:06:46 +0530

    Update EmployeeDAO.java

--- src/main/java/com/ecomerce/webapp/EmployeeDAO.java
+++ src/main/java/com/ecomerce/webapp/EmployeeDAO.java
@@ -15,6 +15,8 @@ public class EmployeeDAO
         list.getEmployeeList().add(new Employee(1, "Wahid", "Khan", "wahid.khan74@gmail.com"));
         list.getEmployeeList().add(new Employee(2, "Alex", "Kolenchiskey", "abc@gmail.com"));
         list.getEmployeeList().add(new Employee(3, "David", "Kameron", "titanic@gmail.com"));
+        list.getEmployeeList().add(new Employee(4, "Sam", "Kameron", "sam@gmail.com"));
+        list.getEmployeeList().add(new Employee(5, "John", "Smith", "john@gmail.com"));
     }
      
     public Employees getAllEmployees() 
